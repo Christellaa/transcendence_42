9fc2bcbe6d6101cf501944bc87c643a1a91bdb39
Paco Gimeno <pacogim.pro@pm.me>
2025-11-19 11:30:27 +0100

    Added option language local storage waiting to implement db

--- Paco/ssr_spa/srcs/public/modules/options.ts
+++ Paco/ssr_spa/srcs/public/modules/options.ts
@@ -1,22 +1,29 @@
 const langs = ['En', 'Fr', 'Es']
 
-function updateLanguage() {
-	const $languageOption = document.querySelector('span[data-action="updateLanguage"]')
-	let lang = langs[$languageOption?.dataset?.currentoption]
-	if (lang) $languageOption.innerText = `Language ${lang}`
+function updateLanguage(origin: 'init' | 'event') {
+	const $languageOption = document.querySelector('span[data-action="updateLanguage"]') as HTMLSpanElement
+	const currentOption: number | undefined = Number($languageOption?.dataset?.currentoption)
+	let lang
+
+	if (currentOption !== undefined) lang = langs[currentOption] || langs[0]
+
+	if (origin === 'init') lang = localStorage.getItem('lang')
+
+	$languageOption.innerText = `Language (${lang})`
+	localStorage.setItem('lang', lang)
 }
 
 function handleOptionClick(evt: any) {
 	let dataset = evt.target.dataset
 
-	if (dataset.action === 'updateLanguage') updateLanguage()
+	if (dataset.action === 'updateLanguage') updateLanguage('event')
 }
 
 export function initOptionEvents() {
 	document.querySelectorAll<HTMLElement>('span.traverse').forEach($el => {
 		$el.addEventListener('click', handleOptionClick)
 	})
-	updateLanguage()
+	updateLanguage('init')
 }
 
 export function cleanOptionEvents() {
