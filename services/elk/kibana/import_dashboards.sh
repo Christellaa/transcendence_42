ebb0814d72026eb7fe16726dd7dcb5652d82dea4
Christella De Sousa <de_sousa_christella@outlook.com>
2025-11-25 15:15:13 +0100

    Add: elk

---
+++ services/elk/kibana/import_dashboards.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+set -e
+
+until [ "$(curl -s http://localhost:5601/api/status | jq -r '.status.overall.level')" = "available" ] ; do
+  echo "Waiting for Kibana to be ready..."
+  sleep 5
+done
+
+curl -X POST "http://kibana:5601/api/saved_objects/_import?overwrite=true" \
+  -H "kbn-xsrf: kibana" \
+  -F "file=@/usr/share/kibana/imports/source.ndjson"
+
+curl -X POST "http://kibana:5601/api/saved_objects/_import?overwrite=true" \
+  -H "kbn-xsrf: kibana" \
+  -F "file=@/usr/share/kibana/imports/dashboards.ndjson"
