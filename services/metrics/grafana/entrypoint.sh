3c459cfb913cadfcf18e4a682ce0c3bf5d767329
Christella De Sousa <de_sousa_christella@outlook.com>
2025-12-09 13:13:16 +0100

    feat(grafana): automate viewer user creation and set dashboards as home - Add a startup script to automatically create a Grafana user with Viewer role to provide read-only access - Configure Grafana to redirect to the dashboards page upon login

---
+++ services/metrics/grafana/entrypoint.sh
@@ -0,0 +1,27 @@
+#!/bin/sh
+
+set -e
+
+echo "Starting Grafana server..."
+exec grafana-server --homepath=/usr/share/grafana --config=/etc/grafana/grafana.ini web &
+GF_PID=$!
+
+echo "Waiting for Grafana..."
+until curl -ks $GRAFANA_URL_HEALTH; do
+    sleep 2
+done
+
+echo "Creating Grafana user..."
+USER=$(curl -k -X POST $GRAFANA_ADDR/api/admin/users \
+    -u "$GF_SECURITY_ADMIN_USER:$GF_SECURITY_ADMIN_PASSWORD" \
+    -H "Content-Type: application/json" \
+    -d "{
+        \"name\":\"$GRAFANA_USER_NAME\",
+        \"email\":\"$GRAFANA_USER_MAIL\",
+        \"login\":\"$GRAFANA_USER_LOGIN\",
+        \"password\":\"$GRAFANA_USER_PWD\"
+    }")
+
+echo "User creation response: $USER"
+
+wait $GF_PID
