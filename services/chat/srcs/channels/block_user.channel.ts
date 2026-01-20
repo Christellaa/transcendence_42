6cb6c62c728aea952e05cb64ccd0f21d440c1ff8
Christella De Sousa <de_sousa_christella@outlook.com>
2026-01-20 10:14:16 +0100

    feat(chat): block user

--- services/chat/srcs/channels/block_user.channel.ts
+++ services/chat/srcs/channels/block_user.channel.ts
@@ -1,9 +1,10 @@
+import { dbPostQuery } from '../services/db.service'
 import { clientsList } from '../state/clients.state'
 import { BunSocketType } from '../types/bunSocket.type'
 import { ClientType } from '../types/client.type'
 import { SocketDataType } from '../types/socketData.type'
 
-export function blockUserChannel(ws: BunSocketType, data: SocketDataType) {
+export async function blockUserChannel(ws: BunSocketType, data: SocketDataType) {
 	let clientFound: ClientType
 	console.log('Block user')
 	for (let client of clientsList) {
@@ -13,8 +14,18 @@ export function blockUserChannel(ws: BunSocketType, data: SocketDataType) {
 	}
 	console.log('Client Found: ', clientFound)
 	if (clientFound) {
-		//TODO Add to DB the blocked user
-		// If client is blocked properly
+		// if client already blocked, deblock
+		// if client is in friendships or friend_requests, delete those entries and block
+		const res = await dbPostQuery({
+			endpoint: 'dbRun',
+			query: {
+				verb: 'INSERT',
+				sql: 'INSERT INTO blocks (blocker_username, blocked_username) VALUES (?, ?)',
+				data: [ws.data.username, clientFound.username]
+			}
+		})
+		if (res.status >= 400) console.log('status: ', res.status, 'message: ', res.message)
+		console.log('Blocked user added to DB: ', res)
 		data.msg = `User ${clientFound.username} has been blocked`
 		data.type = 'notification'
 		data.notificationLevel = 'error'
